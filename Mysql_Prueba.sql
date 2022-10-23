9e519a8146957ff132dfd99b8ee9bf8671f53e24
FrankGalloSanchez <111780777+FrankGalloSanchez@users.noreply.github.com>
2022-10-22 23:50:49 -0500

    2da version

--- Mysql_Prueba.sql
+++ Mysql_Prueba.sql
@@ -1,4 +1,18 @@
 /*Conectar Databases*/ 
 SHOW DATABASES;
 /*Crear base de datos*/ 
- CREATE DATABASE DbMinimarket;  
+ CREATE DATABASE DbMinimarket;
+
+ /*Poner en uso base de datos*/
+USE dbminimarket; 
+
+ /*Crear tabla*/
+CREATE TABLE VENDEDOR (
+    CODVEND CHAR(6),
+    NOMVEND VARCHAR(50),
+    APEVEND VARCHAR(80)),
+    DOCVEND VARCHAR(10),
+    CELVEND CHAR(9),
+    DOMVEND VARCHAR(80),
+    PRIMARY KEY (CODVEND)
+);
